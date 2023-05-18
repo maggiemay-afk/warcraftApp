9e53247f6bd7a63d50a1c923675833f7884c66b4
maggiemay-afk <hermsmk@gmail.com>
2023-05-17 20:40:36 -0400

    added mount file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,27 @@
 import express, { Application, Request, Response } from 'express'
+import axios, {isCancel, AxiosError} from 'axios';
 
 const app: Application = express()
-
 const port: number = 3001
+let token: string;
 
 app.get('/toto', (req: Request, res: Response) => {
     res.send('Hello toto')
+})
+
+app.get('/new-mount', (req: Request, res: Response) => {
+    
+    if (token === null) {
+        axios.post("https://oauth.battle.net/token", new URLSearchParams({
+            'grant_type': 'client_credentials'
+        }), 
+        {
+            auth: {
+                username: 'client_id',
+                password: 'client_secret'
+            }
+        })
+    }
 })
 
 app.listen(port, function () {
