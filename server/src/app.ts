9e78b18e1ea58a31b9863a9e5dda02225fd14fd0
maggiemay-afk <hermsmk@gmail.com>
2023-05-24 21:06:11 -0400

    implemented getToken function, added new-mount route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,29 +1,66 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express';
 import axios, {isCancel, AxiosError} from 'axios';
+require('dotenv').config();
 
-const app: Application = express()
-const port: number = 3001
+const app: Application = express();
+const port: number = 3001;
 let token: string;
 
-app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
+const getToken = async (): Promise<string> => { //async function that returns a Promise of type string
+    console.log("token is falsey");
+
+    const response = await axios.post("https://oauth.battle.net/token", new URLSearchParams({
+        'grant_type': 'client_credentials'
+    }), 
+    {
+        auth: {
+            username: process.env.WOW_CLIENT_ID!,
+            password: process.env.WOW_CLIENT_SECRET!
+        }
+
+    }).catch((error) => {
+        console.log(error);
+        throw error;
+    }); 
+    
+    console.log(response); //api POST response
+
+    token = response.data.access_token;
+    console.log(token) //check variable is set
+
+    return token;
+
+}
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('welcome home');
 })
 
-app.get('/new-mount', (req: Request, res: Response) => {
+app.get('/new-mount', async (req: Request, res: Response) => {
+    console.log('In new-mount');
     
-    if (token === null) {
-        axios.post("https://oauth.battle.net/token", new URLSearchParams({
-            'grant_type': 'client_credentials'
-        }), 
-        {
-            auth: {
-                username: 'client_id',
-                password: 'client_secret'
-            }
-        })
+    if (!token) {
+        token = await getToken();
     }
+
+    console.log("token is truthy");
+
+    const response = await axios.get('https://us.api.blizzard.com/data/wow/token/', {
+        params: {
+            'namespace': 'dynamic-us'
+        },
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    }).catch(function (error) {
+        console.log(error);
+        throw error;
+    });
+
+    console.log(response); //api GET response
+
 })
 
 app.listen(port, function () {
-    console.log(`App is listening on port ${port} !`)
+    console.log(`App is listening on port ${port} !`);
 })
