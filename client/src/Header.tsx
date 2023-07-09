ea6ccf7cd1b62a646c89545b5f03677c705037da
maggiemay-afk <hermsmk@gmail.com>
2023-07-09 14:41:10 -0400

    Started adding fuzzy sort

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -10,14 +10,15 @@ import IconButton from '@mui/material/IconButton';
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Mount Guesser
-          </Typography>
-          <Button color="inherit">About</Button>
-        </Toolbar>
-      </AppBar>
+        <img src='../public/halfhill.PNG'></img>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Mount Guesser
+                </Typography>
+                <Button color="inherit">About</Button>
+            </Toolbar>
+        </AppBar>
     </Box>
   );
 }
--- client/src/MountImage.tsx
+++ client/src/MountImage.tsx
@@ -1,14 +1,17 @@
 import { error } from 'console';
 import React, { Component, useState, useEffect } from 'react';
 import {TextField, Button} from '@mui/material/';
+import fuzzysort from 'fuzzysort'
 
 type ImageResponse = {
   image: string,
   name: string
 }
 
+
 export default function MountImage() {
   const [image, setImage] = useState<ImageResponse|undefined>();
+  const [guessValue, setGuessValue] = useState<string>("");
 
   useEffect(() => {
     
@@ -24,6 +27,24 @@ export default function MountImage() {
       
   }, []);
 
+  function checkAnswer(): boolean {
+    console.log(guessValue)
+
+    if(!image) {
+      return false;
+    }
+
+    const result = fuzzysort.single(guessValue, image.name)
+
+    if (!result || result.score != 0) {
+      return false;
+    }
+    
+    console.log(result)
+    return true;
+
+  }
+
   if (!image) {
     return (
       <p>loading...</p>
@@ -33,11 +54,22 @@ export default function MountImage() {
   return (
     <div>
 
-      <img src={image.image}></img>
+      <img id='mountImage' src={image.image}></img>
       <p>Answer: {image.name}</p>
-      <TextField id="outlined-basic" label="Answer" variant="outlined"/>
-      <Button variant="outlined" size="large">Submit</Button>
-      
+      <div id='userInput'>
+        <TextField 
+            id="outlined-basic" 
+            label="Answer" 
+            variant="outlined"
+            value={guessValue}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setGuessValue(event.target.value)
+            }}
+        />
+        <Button variant="outlined" size="large" onClick={checkAnswer}>
+          Submit
+        </Button>
+      </div>
     </div>
   )
 }
